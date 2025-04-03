16c381165aa0230d1a3737da45420c90a1009e62
Jinge Zhang <jinge_windows@hotmail.com>
2025-04-04 04:11:06 +1100

    Add project files.

---
+++ WPF_WebScreenSaver_Project/App.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_WebScreenSaver_Project
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //程序启动参数，当屏幕保护程序被设置的时候，启动设置页面；运行程序保护程序的时候，启动运行页面。
+        private StartupEventArgs _startupEventArgs;
+        public static bool IsSettingMode = false;
+        public App()
+        {
+
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            string[] args = e.Args;
+            if (args.Length > 0)
+            {
+                string firstArgument = args[0].ToLower().Trim();
+                string secondArgument = null;
+
+                // Handle cases where arguments are separated by colon.
+                // Examples: /c:1234567 or /P:1234567
+                if (firstArgument.Length > 2)
+                {
+                    //secondArgument = firstArgument.Substring(3).Trim();
+                    firstArgument = firstArgument.Substring(0, 2);
+                }
+                switch (firstArgument)
+                {
+                    case "/c":  // Configuration mode
+                        IsSettingMode = true;
+                        break;
+                    case "/p":  // Preview mode
+                        throw new NotImplementedException();
+                    case "/s":  // Full-screen mode
+                        break;
+                    default:
+                        break;
+                }
+            }
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ WPF_WebScreenSaver_Project/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_WebScreenSaver_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string js1 = "var list=document.querySelectorAll('.setting-hide');list.forEach(x=>x.style.display='block');";
+        private string js2 = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            if(!App.IsSettingMode)
+            {
+                this.WindowStyle = WindowStyle.None;
+            }
+            this.PreviewKeyDown += new KeyEventHandler(EscKeyDown);
+            Run();
+
+        }
+
+        public async void Run()
+        {
+            string binPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string web_Index_FilePath = "Web/Web_index.html";
+            binPath = binPath.Replace("\\","/");
+            string fullPath = binPath + web_Index_FilePath;
+
+            await WebView.EnsureCoreWebView2Async();
+            //WebView.CoreWebView2.Navigate("file:///C:/Users/jinge/Desktop/HTML&CSS/2025-Swiperjs/index.html");
+            //WebView.CoreWebView2.Navigate("http://127.0.0.1:5500/index.html");
+            WebView.CoreWebView2.Navigate(fullPath);
+            WebView.NavigationCompleted += async (sender, e) =>
+            {
+                if (e.IsSuccess)
+                {
+                    if(App.IsSettingMode)
+                    {
+                        await ((Microsoft.Web.WebView2.Wpf.WebView2)sender).ExecuteScriptAsync(js1);
+                    }
+                    #region removed test
+                    //await ((Microsoft.Web.WebView2.Wpf.WebView2)sender).ExecuteScriptAsync("document.querySelector('.swiper-slide').innerHTML='OK'");
+                    //while (true)
+                    //{
+                    //    await Task.Delay(1000);
+                    //    await ((Microsoft.Web.WebView2.Wpf.WebView2)sender).ExecuteScriptAsync("document.querySelector('#s1').style.backgroundColor ='aquamarine'");
+                    //    await Task.Delay(1000);
+                    //    await ((Microsoft.Web.WebView2.Wpf.WebView2)sender).ExecuteScriptAsync("document.querySelector('#s1').style.backgroundColor ='bisque'");
+
+                    //}
+                    #endregion
+                }
+            };
+        }
+
+        private void EscKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                Close();
+            }
+        }
+    }
+}
